1e955b04bb9a4e9e7a11a42d29de55c6aca2cc96
kxfo12 <pytajnikpytajnik2@gmail.com>
2024-09-04 15:58:05 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("siemano kolano");
+        //typ prosty - tylko wartość - mała litera
+        //typ złożony - własności + metody - wielka litera
+        double wylosowanaLiczba = Math.random()*10+1;
+        System.out.println(wylosowanaLiczba);
     }
 }
