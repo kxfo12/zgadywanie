828eb68c3fa879e3306a8d940eeb4623404fadbf
kxfo12 <pytajnikpytajnik2@gmail.com>
2024-09-04 15:45:05 +0200

    dodatkowy komunikat

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     //nazwa klasy ta sama co nazwa pliku, z wielkiej litery
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("siemano kolano");
     }
 }
