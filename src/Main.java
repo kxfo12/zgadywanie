c59253ef78edac0f665590bace2c9b996a245b12
kxfo <147588035+kxfo12@users.noreply.github.com>
2024-09-05 13:20:49 +0200

    pętle

--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,73 @@ public class Main {
         //typ złożony - własności + metody - wielka litera
         int wylosowanaLiczba = (int)(Math.random()*100)+1;//rzutowanie
         System.out.println(wylosowanaLiczba);
-        System.out.println("zgadnij liczbe");
-        Scanner klawiatura = new Scanner(System.in);
-        int wpisanaliczba = klawiatura.nextInt();
+        int wpisanaliczba = 0;
+        /*for (int i = 0; i < 10; i++){
+            System.out.println("zgadnij liczbe");
+            Scanner klawiatura = new Scanner(System.in);
+            wpisanaliczba = klawiatura.nextInt();
 
-        if(wpisanaliczba == wylosowanaLiczba){
-            System.out.println("gratulacje wygrałeś");
-        }
-        else{
-            System.out.println("nie udało się");
-        }
-        int roznica = wylosowanaLiczba>wpisanaliczba? wylosowanaLiczba - wpisanaliczba : wpisanaliczba - wylosowanaLiczba;
-        if(roznica != 0){
-            System.out.println(roznica);
-        }
-        //roznica = roznica/10;//dzielnica calkowita
-        switch(roznica) {
-            case 0:
-                System.out.println("dobra robota");
+            if (wpisanaliczba == wylosowanaLiczba) {
+                System.out.println("gratulacje wygrałeś");
                 break;
-            case 1:
-                System.out.println("bardzo blisko");
-                break;
-            case 2:
-                System.out.println("blisko");
-                break;
-            default:
-                System.out.println("cieńko");
-                break;
+            } else {
+                System.out.println("nie udało się");
+            }
+            int roznica = wylosowanaLiczba > wpisanaliczba ? wylosowanaLiczba - wpisanaliczba : wpisanaliczba - wylosowanaLiczba;
+            if (roznica != 0) {
+                System.out.println(roznica);
+            }
+            //roznica = roznica/10;//dzielnica calkowita
+            switch (roznica) {
+                case 0:
+                    System.out.println("dobra robota");
+                    break;
+                case 1:
+                    System.out.println("bardzo blisko");
+                    break;
+                case 2:
+                    System.out.println("blisko");
+                    break;
+                default:
+                    System.out.println("cieńko");
+                    break;
+            }
+        }*/
+        while(wpisanaliczba != wylosowanaLiczba){
+            System.out.println("zgadnij liczbe");
+            Scanner klawiatura = new Scanner(System.in);
+            wpisanaliczba = klawiatura.nextInt();
+
+            if (wpisanaliczba == wylosowanaLiczba) {
+                System.out.println("gratulacje wygrałeś");
+            } else {
+                System.out.println("nie udało się");
+            }
+            int roznica = wylosowanaLiczba > wpisanaliczba ? wylosowanaLiczba - wpisanaliczba : wpisanaliczba - wylosowanaLiczba;
+            if (roznica != 0) {
+                System.out.println(roznica);
+            }
+            //roznica = roznica/10;//dzielnica calkowita
+            switch (roznica) {
+                case 0:
+                    System.out.println("dobra robota");
+                    break;
+                case 1:
+                    System.out.println("bardzo blisko");
+                    break;
+                case 2:
+                    System.out.println("blisko");
+                    break;
+                default:
+                    System.out.println("cieńko");
+                    break;
+            }
         }
+        /*System.out.println(switch (roznica){
+            case 0 -> "dobra robota";
+            case 1 -> "bardzo blisko";
+            case 2 -> "blisko";
+            default -> "cieńko";
+        });*/
     }
 }
